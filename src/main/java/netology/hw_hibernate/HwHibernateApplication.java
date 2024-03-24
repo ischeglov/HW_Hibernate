0c12e88b1c04888e94f822afe220bd1a2debde92
imshcheglov <Ivan_mix@mail.ru>
2024-03-24 21:04:08 +0500

    Initial commit

---
+++ src/main/java/netology/hw_hibernate/HwHibernateApplication.java
@@ -0,0 +1,13 @@
+package netology.hw_hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HwHibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/netology/hw_hibernate/HwHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package netology.hw_hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
