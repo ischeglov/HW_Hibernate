639e069924d3136f3d1f88741066559dee0dae54
imshcheglov <Ivan_mix@mail.ru>
2024-04-06 21:34:38 +0500

    add basic logic

--- src/main/java/netology/hw_hibernate/config/SecurityConfig.java
+++ src/main/java/netology/hw_hibernate/config/SecurityConfig.java
@@ -1,28 +1,47 @@
 package netology.hw_hibernate.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true, jsr250Enabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
-                .withUser("Ivan").password("{noop}Ivan260").authorities("age", "name");
+                .withUser("Ivan")
+                .password(encoder().encode("Ivan260"))
+                .roles("READ", "WRITE");
+        auth.inMemoryAuthentication()
+                .withUser("Anna")
+                .password(encoder().encode("Anna"))
+                .roles("READ");
+        auth.inMemoryAuthentication()
+                .withUser("Arseny")
+                .password(encoder().encode("Arseny"))
+                .roles("DELETE");
     }
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.formLogin()
-                .and()
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.formLogin().and()
                 .authorizeRequests().antMatchers("/persons/by-city").permitAll()
                 .and()
-                .authorizeRequests().antMatchers("/persons/by-age").hasAuthority("age")
+                .authorizeRequests().antMatchers("/persons/by-age").hasRole("WRITE")
                 .and()
-                .authorizeRequests().antMatchers("/persons/by-name").hasAuthority("name")
+                .authorizeRequests().antMatchers("/persons/by-name").hasAnyRole("DELETE","WRITE")
                 .and()
                 .authorizeRequests().anyRequest().authenticated();
     }
--- src/main/java/netology/hw_hibernate/controller/PersonController.java
+++ src/main/java/netology/hw_hibernate/controller/PersonController.java
@@ -4,10 +4,13 @@ package netology.hw_hibernate.controller;
 import lombok.AllArgsConstructor;
 import netology.hw_hibernate.entity.Person;
 import netology.hw_hibernate.service.PersonService;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.security.RolesAllowed;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,18 +21,27 @@ public class PersonController {
     private PersonService personService;
 
     @GetMapping("/persons/by-city")
+    @Secured({"ROLE_READ"})
     public List<Person> getPersonsByCity(@RequestParam(value = "city", required = false) String city) {
         return personService.getPersonsByCity(city);
     }
 
     @GetMapping("/persons/by-age")
+    @RolesAllowed({"ROLE_WRITE"})
     public List<Person> getPersonsByAgeLess(@RequestParam(value = "age", required = false) int age) {
         return personService.getPersonsByAgeLess(age);
     }
 
     @GetMapping("/persons/by-name")
+    @PreAuthorize("hasRole('ROLE_WRITE') or hasRole('ROLE_DELETE')")
     public Optional<Person> getPersonByNameAndSurname(@RequestParam(value = "name", required = false) String name,
                                                       @RequestParam(value = "surname", required = false) String surname) {
         return personService.getPersonByNameAndSurname(name, surname);
+    }
+
+    @GetMapping("/persons/hi")
+    @PreAuthorize("#username == authentication.principal.username")
+    public String hiUser(String username) {
+        return "Hi " + username;
     }
 }
